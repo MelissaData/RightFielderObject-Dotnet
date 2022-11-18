7a82983a921bf132aaaec7d8d4dcb4e4fc2eb378
Melissa <erik@melissadata.com>
2022-11-18 09:53:34 -0800

    Tested successfully using Melissa data files for 2022.10

--- MelissaDataRightFielderObjectWindowsNETExample.ps1
+++ MelissaDataRightFielderObjectWindowsNETExample.ps1
@@ -18,7 +18,7 @@ class DLLConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2022.09'
+$RELEASE_VERSION = '2022.10'
 $ProductName = "RF_DATA"
 
 # Uses the location of the .ps1 file 
