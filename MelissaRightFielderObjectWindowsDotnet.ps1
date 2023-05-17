60d98fbbd58edf01424279634a7f98bb1605e678
Melissa <>
2023-05-17 13:45:42 -0700

    Tested successfully using Melissa data files for 2023.05

--- MelissaRightFielderObjectWindowsDotnet.ps1
+++ MelissaRightFielderObjectWindowsDotnet.ps1
@@ -18,7 +18,7 @@ class DLLConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2023.04'
+$RELEASE_VERSION = '2023.05'
 $ProductName = "RF_DATA"
 
 # Uses the location of the .ps1 file 
