056e280beadfafa831a71e94c549fbe1f1406da9
Melissa <>
2025-05-13 14:42:47 -0700

    Tested successfully using Melissa data files for 2025.Q2

--- MelissaRightFielderObjectWindowsDotnet.ps1
+++ MelissaRightFielderObjectWindowsDotnet.ps1
@@ -18,7 +18,7 @@ class FileConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2025.Q1'
+$RELEASE_VERSION = '2025.Q2'
 $ProductName = "RF_DATA"
 
 # Uses the location of the .ps1 file 
